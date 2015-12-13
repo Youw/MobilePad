02792ee04629634b2b3d3815b05450f5d1e1eb24
Gasper <bpaul2004@gmail.com>
2015-12-13 22:38:33 +0200

    added simple networking for testing server

--- app/src/main/java/com/easyapp/mobilepad/Connections.java
+++ app/src/main/java/com/easyapp/mobilepad/Connections.java
@@ -1,16 +1,31 @@
 package com.easyapp.mobilepad;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Connections extends ActionBarActivity {
+
+    EditText textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connections);
+
+        textView = (EditText)findViewById(R.id.result_text);
+
     }
 
     @Override
@@ -33,5 +48,45 @@ public class Connections extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendPacket(View v){
+        String outText = textView.getText().toString();
+        try {
+            String inText = new SendData().execute(outText).get();
+            textView.setText(inText == null ? "Failed to receive data or null" : inText);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public class SendData extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            Socket socket = null;
+            DataInputStream in = null;
+            DataOutputStream out = null;
+            String result = null;
+
+            try {
+                socket = new Socket("192.168.1.36", 8887);
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+                out.writeBytes(params[0]+"\n");
+                result = in.readUTF();
+            } catch (Exception e){
+                result = e.getMessage();
+            }
+            finally {
+                if(socket!=null){
+                    try{
+                        socket.close();
+                    } catch (IOException e) {
+                        result += e.getMessage();
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
